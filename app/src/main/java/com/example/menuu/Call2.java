e6bd0671f9a2989db34c0d0d908bd64146e67c79
mobile <mail>
2022-09-23 15:13:07 +0500

    menushka

---
+++ app/src/main/java/com/example/menuu/Call2.java
@@ -0,0 +1,14 @@
+package com.example.menuu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Call2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call2);
+    }
+}
